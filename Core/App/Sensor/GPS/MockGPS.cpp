54025876a4cc26c87f22826feb37c6760bed9352
Artem Stukalenko <stukalenko.artem@gmail.com>
2026-07-23 17:32:20 +0300

    Added MockGPS

---
+++ Core/App/Sensor/GPS/MockGPS.cpp
@@ -0,0 +1,24 @@
+#include "MockGPS.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+MockGPS::MockGPS() : latitude(37.7749), longitude(-122.4194), altitude(10.0f) {
+	stringBuffer[0] = '\0';
+}
+
+bool MockGPS::init() {
+	return true;
+}
+
+void MockGPS::update() {
+	latitude  = -90.0  + static_cast<double>(rand()) / RAND_MAX * 180.0;
+	longitude = -180.0 + static_cast<double>(rand()) / RAND_MAX * 360.0;
+	altitude  = static_cast<float>(rand() % 10000);
+
+	snprintf(stringBuffer, sizeof(stringBuffer), "GPS -> LAT: %d LON: %d AL: %d",
+			(int) latitude, (int) longitude, (int) altitude);
+}
+
+const char* MockGPS::getDataString() {
+	return stringBuffer;
+}
---
+++ Core/App/Sensor/GPS/MockGPS.h
@@ -0,0 +1,24 @@
+#ifndef MOCKGPS_H
+#define MOCKGPS_H
+
+#include "Sensor/ISensor.h"
+
+class MockGPS : public ISensor {
+
+private:
+	double latitude;
+	double longitude;
+	float altitude;
+	char stringBuffer[64];
+
+public:
+	MockGPS();
+
+	~MockGPS() override = default;
+
+	bool init() override;
+	void update() override;
+	const char* getDataString() override;
+};
+
+#endif
